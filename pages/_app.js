2807ffd866eaaf2002de1c5b6ef9148a77a579f9
OMAGE MICHEAL BRIGHT <micheal.omage@gmail.com>
2022-11-05 08:39:42 +0100

    new project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import {Fragment} from "react"
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <link rel="shortcut icon" href="vote.png" type="image/x-icon" />
+        <title>Polling Units</title>
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
 
 export default MyApp
